cbfb2b41e4f52c2e52cbd13149fd11db8eda21ac
selin ağan <80126808+selinbsa@users.noreply.github.com>
2022-08-23 14:45:16 +0300

    changed

--- app/src/main/java/com/example/disher/MainActivity.kt
+++ app/src/main/java/com/example/disher/MainActivity.kt
@@ -3,15 +3,29 @@ package com.example.disher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.disher.category.viewmodel.MainViewModel
 import com.example.disher.navigation.DisherNavigation
 import com.example.disher.ui.theme.DisherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepVisibleCondition{
+                viewModel.isLoading.value
+            }
+
+        }
+
         setContent {
             DisherTheme {
                 DisherApp()
